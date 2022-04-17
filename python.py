dfae05e25a76ba92de36794e196e6f470c9ac810
EduardoMontero-100 <ejmontero10@gmail.com>
2022-04-17 12:21:56 -0300

    add scripts

--- python.py
+++ python.py
@@ -8,3 +8,13 @@ print('Today plus 3: ' +str(today_3))
 colors = ['blue', 'black', 'red', 'orange']
 for color in colors:
     print(color)
+
+dict_colors ={0:'blue', 1:'black', 2:'red', 3:'orange'}
+for key, value in dict_colors.items():
+    print('El valor correspondiente a '+str(key)+' es: '+str(value))
+
+x1, x2, x3, x4 = colors
+print('Color 1: '+x1)
+print('color 2: '+x2)
+print('color 3: '+x3)
+print('color 4: '+x4)
